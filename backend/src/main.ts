d0b3875f785b71e10b76a59f36cca7b5f65c033d
vasanthicreationsoffical <vasanthi.creationsoffical@gmail.com>
2026-08-29 13:05:17 +0530

    bump: main.ts v1.0.3 to trace Railway deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,4 @@
-console.log('[STARTUP] Executing main.ts module script v1.0.2 (Omnichannel & Inventory Velocity Analytics Active)...');
+console.log('[STARTUP] Executing main.ts module script v1.0.3 (Omnichannel & Barcode Patch Active)...');
 
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
